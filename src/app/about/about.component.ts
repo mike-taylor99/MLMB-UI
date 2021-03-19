cfc54f7bd878ff1000a02a1a2346e4512e6dd630
Michael Taylor <michael.a.taylor@uconn.edu>
2021-03-19 13:28:51 -0400

    add Ensemble classifier

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -16,7 +16,8 @@ export class AboutComponent implements OnInit {
     {'model':'Logistic Regression', 'accuracy':'70.13%', 'description':'penalty=elasticnet, solver=saga, l1_ratio=0.5'},
     {'model':'Support Vector Classifier', 'accuracy':'68.28%', 'description':'kernel=linear'},
     {'model':'Random Forest Classifier', 'accuracy':'70.13%', 'description':'bootstrap: False, min_samples_leaf: 3, n_estimators: 50, min_samples_split: 10, max_features: sqrt, max_depth: 20'},
-    {'model':'Gradient Boosting Classifier', 'accuracy':'71.12%', 'description':'loss: exponential, n_estimators:100, min_samples_leaf: 3, min_samples_split: 10, max_features: sqrt, max_depth: 20'}
+    {'model':'Gradient Boosting Classifier', 'accuracy':'71.12%', 'description':'loss: exponential, n_estimators:100, min_samples_leaf: 3, min_samples_split: 10, max_features: sqrt, max_depth: 20'},
+    {'model':'Ensemble', 'accuracy':'75.75%', 'description':'average prediction among all models'}
   ];
 
 }
